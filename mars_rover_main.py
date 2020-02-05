c85280227ebc7d5afba9abb490e0eb2869283198
emilybradley1994 <57965445+emilybradley1994@users.noreply.github.com>
2020-02-05 21:52:31 +0000

    Add files via upload

---
+++ mars_rover_main.py
@@ -0,0 +1,114 @@
+class Plateau():
+     
+    
+    
+    def __init__(self, height, width, min_height = 0, min_width = 0):        
+        self.height = height
+        self.width = width
+        self.min_height = min_height
+        self.min_width = min_width
+                
+
+    def check_move_available(self, rover):        
+        return rover.X > self.width or rover.Y > self.height or rover.X < self.min_width or rover.Y < self.min_height
+
+
+    
+
+class MarsRover():
+    
+    RIGHT_ROTATE = {
+        'N':'E',
+        'E':'S',
+        'S':'W',
+        'W':'N'
+    }
+
+    LEFT_ROTATE = {
+        'N':'W',
+        'W':'S',
+        'S':'E',
+        'E':'N'
+    }
+    
+    
+
+    def __init__(self, X, Y, heading, plateau):
+        self.X = X
+        self.Y = Y
+        self.heading = heading
+        self.plateau = plateau
+        
+        
+    def run_instructions(self, instruction):
+        if instruction == 'L':
+            self.rotate_left()
+        elif instruction == 'R':
+            self.rotate_right()
+        elif instruction == 'M':
+            self.move()
+    
+    
+    def process(self, instructions):
+        for i in range(len(instructions)):
+            self.run_instructions(instructions[i])
+
+
+    def rotate_right(self):
+        self.heading = self.RIGHT_ROTATE[self.heading]
+                         
+    
+    def rotate_left(self):
+        self.heading = self.LEFT_ROTATE[self.heading]
+            
+            
+    def move(self):
+                
+        if self.heading == 'N':
+            self.Y += 1
+        elif self.heading == 'E':
+            self.X += 1
+        elif self.heading == 'S':
+            self.Y -= 1    
+        elif self.heading == 'W':
+            self.X -= 1
+            
+    def __str__(self):        
+        return str(self.X) + " " + str(self.Y) + " " + self.heading
+    
+    
+    
+
+    
+        
+        
+def main():
+
+    plateau_size = input("Plateau size:")
+    height = int(plateau_size[0])
+    width = int(plateau_size[2])
+    plateau = Plateau(height, width)
+    '''Set plateau'''
+    
+    while True:
+        position = input("Position:")
+        while True:
+            X = int(position[0])
+            Y = int(position[2])
+            direction = position[4]
+            rover = MarsRover(X, Y, direction, plateau)
+            instructions = input("Please input directions for rover.")
+            rover.process(instructions)
+            if plateau.check_move_available(rover):
+                print ("You cannot move the rover here.") 
+            else:
+                print (rover)
+                break
+    
+    
+
+if __name__ == '__main__':
+    
+    main()
+            
+  
